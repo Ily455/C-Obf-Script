3500ae49408e1cecdf2755062719a9ae08800c0f
Ily455 <63756815+Ily455@users.noreply.github.com>
2024-05-12 11:14:23 +0000

    modified:   obfuscator.py

--- obfuscator.py
+++ obfuscator.py
@@ -23,25 +23,25 @@ keywords={"alignas", "alignof", "auto", "bool", "break", "case", "char", "const"
 
 preprocessors = {
     "if", "elif", "else", "endif","ifdef", "ifndef", "elifdef", "elifndef", "define", "undef",
-    "include", "embed", "line", "error", "warning", "pragma", "defined", "__has_include", 
+    "include", "embed", "line", "error", "warning", "pragma", "defined", "__has_include",
     "__has_embed", "__has_c_attribute"}
 
 standard_functions = {
-    "abort", "abs", "acos", "asin", "atan", "atan2", "atexit", "atof", "atoi", "atol", "bsearch", 
-    "calloc", "ceil", "clearerr", "clock", "cos", "cosh", "ctime", "difftime", "div", "exit", 
-    "exp", "fabs", "fclose", "feof", "ferror", "fflush", "fgetc", "fgetpos", "fgets", "floor", 
-    "fmod", "fopen", "fprintf", "fputc", "fputs", "fread", "free", "freopen", "frexp", "fscanf", 
-    "fseek", "fsetpos", "ftell", "fwrite", "getc", "getchar", "getenv", "gets", "gmtime", 
-    "isalnum", "isalpha", "iscntrl", "isdigit", "isgraph", "islower", "isprint", "ispunct", 
-    "isspace", "isupper", "isxdigit", "labs", "ldexp", "ldiv", "localeconv", "localtime", "log", 
-    "log10", "longjmp", "malloc", "mblen", "mbstowcs", "mbtowc", "memchr", "memcmp", "memcpy", 
-    "memmove", "memset", "mktime", "modf", "perror", "pow", "printf", "putc", "putchar", 
-    "puts", "qsort", "raise", "rand", "realloc", "remove", "rename", "rewind", "scanf", 
-    "setbuf", "setjmp", "setlocale", "setvbuf", "signal", "sin", "sinh", "sprintf", "sqrt", 
-    "srand", "sscanf", "strcat", "strchr", "strcmp", "strcoll", "strcpy", "strcspn", "strerror", 
-    "strftime", "strlen", "strncat", "strncmp", "strncpy", "strpbrk", "strrchr", "strspn", 
-    "strstr", "strtod", "strtok", "strtol", "strtoul", "strxfrm", "system", "tan", "tanh", 
-    "time", "tmpfile", "tmpnam", "tolower", "toupper", "ungetc", "vfprintf", "vprintf", 
+    "abort", "abs", "acos", "asin", "atan", "atan2", "atexit", "atof", "atoi", "atol", "bsearch",
+    "calloc", "ceil", "clearerr", "clock", "cos", "cosh", "ctime", "difftime", "div", "exit",
+    "exp", "fabs", "fclose", "feof", "ferror", "fflush", "fgetc", "fgetpos", "fgets", "floor",
+    "fmod", "fopen", "fprintf", "fputc", "fputs", "fread", "free", "freopen", "frexp", "fscanf",
+    "fseek", "fsetpos", "ftell", "fwrite", "getc", "getchar", "getenv", "gets", "gmtime",
+    "isalnum", "isalpha", "iscntrl", "isdigit", "isgraph", "islower", "isprint", "ispunct",
+    "isspace", "isupper", "isxdigit", "labs", "ldexp", "ldiv", "localeconv", "localtime", "log",
+    "log10", "longjmp", "malloc", "mblen", "mbstowcs", "mbtowc", "memchr", "memcmp", "memcpy",
+    "memmove", "memset", "mktime", "modf", "perror", "pow", "printf", "putc", "putchar",
+    "puts", "qsort", "raise", "rand", "realloc", "remove", "rename", "rewind", "scanf",
+    "setbuf", "setjmp", "setlocale", "setvbuf", "signal", "sin", "sinh", "sprintf", "sqrt",
+    "srand", "sscanf", "strcat", "strchr", "strcmp", "strcoll", "strcpy", "strcspn", "strerror",
+    "strftime", "strlen", "strncat", "strncmp", "strncpy", "strpbrk", "strrchr", "strspn",
+    "strstr", "strtod", "strtok", "strtol", "strtoul", "strxfrm", "system", "tan", "tanh",
+    "time", "tmpfile", "tmpnam", "tolower", "toupper", "ungetc", "vfprintf", "vprintf",
     "vsprintf"
 }
 
@@ -71,24 +71,29 @@ excluded=keywords.union(preprocessors.union(backslashes.union(standard_functions
 ###FUNCTIONS###
 
 def remove_headers(code):
-    removed_header = []
-    removed_lib = []
-    removed_macros = []
-    lib_pattern = r'^\s*#(include).*?$'
-    macros_pattern = r'^\s*#(define|ifdef|ifndef).*?$'
-    header_pattern = r'^\s*#(include|define|undef|if|ifdef|ifndef|else|elif|endif|line|error|pragma).*?$'
-    code = re.sub(lib_pattern, lambda match: removed_lib.append(match.group(0)), code, flags=re.MULTILINE)
-    # code = re.sub(macros_pattern, lambda match: removed_macros.append(match.group(0)), code, flags=re.MULTILINE)
-    # code = re.sub(header_pattern, lambda match: removed_header.append(match.group(0)), code, flags=re.MULTILINE)
-    
-    return code, removed_header, removed_lib, removed_macros
+
+  pattern = r"""^\s*#(include|define|undef|if|ifdef|ifndef|else|elif|endif|line|error|pragma).*?$"""
+  removed_lines, removed_header, removed_lib, removed_macro = [], [], [], []
+  for match in re.finditer(pattern, code, flags=re.MULTILINE):
+    removed_lines.append(match.group(0))
+    kind = match.group(1)
+    if kind in ("include",):
+      removed_lib.append(match.group(0).strip("\n"))
+    elif kind in ("define", "ifdef", "ifndef"):
+      removed_macro.append(match.group(0).strip("\n"))
+    else:
+      removed_header.append(match.group(0).strip("\n"))
+
+  code = "".join([line for line in code.splitlines() if line not in removed_lines])
+
+  return code, removed_header, removed_lib, removed_macro
 
 def add_headers(code, removed_header):
     def delete_empty_lines(text):
         lines = text.split('\n')
         non_empty_lines = [line for line in lines if line.strip()]
         result = '\n'.join(non_empty_lines)
-        
+
         return result
     headers = '\n'.join(removed_header)
     return delete_empty_lines(headers + '\n' + code)
@@ -120,7 +125,7 @@ def replace_identifiers(code):
                 tokens = declaration.strip().split()
                 variables.add(tokens[0])
         return variables
-    
+
     def rename_vars(code, variable_mapping):
         pattern = r'"(?:\\.|[^"\\])*?"'
         qsp_texts = re.findall(pattern, code)
@@ -150,14 +155,14 @@ def replace_identifiers(code):
             code = code.replace(replacement, variable)
             # print(variable)
         return code
-    
+
     def map_vars(vars):
         variable_mapping = {}
         for variable in vars:
             random_string = ''.join(random.choice(string.ascii_letters) for _ in range(32))
             variable_mapping[variable] = random_string
         return variable_mapping
-    
+
     codeVars=find_variables(code)
     # print(codeVars)
     map=map_vars(codeVars)
@@ -172,7 +177,9 @@ def preprocess_code(code):
     code = replace_identifiers(code)
     # code = add_headers(code, removed_header)
     code = add_headers(code, removed_lib)
+    print(removed_lib)
     print(removed_header)
+    print(removed_macros)
     return code
 
 if __name__ == "__main__":
@@ -192,9 +199,9 @@ if __name__ == "__main__":
     processed_code = preprocess_code(c_code)
 
     output_dir = os.path.dirname(input_file)
-    
+
     output_file = os.path.join(output_dir, 'obfuscated_' + os.path.basename(input_file))
-    
+
     with open(output_file, 'w') as file:
         file.write(processed_code)
 
@@ -218,25 +225,25 @@ if __name__ == "__main__":
 
 #     # Combine predefined keywords, constants, and types
 #     exclude_identifiers = predefined_keywords.union(predefined_constants, predefined_types)
-    
+
 #     # Generate a mapping of identifiers to replacement strings
 #     identifier_map = {}
 #     for identifier in re.findall(r'\b[A-Za-z_][A-Za-z0-9_]*\b', code):
 #         if identifier not in exclude_identifiers:
 #             if identifier not in identifier_map:
 #                 identifier_map[identifier] = ''.join(random.choice(string.ascii_letters) for _ in range(len(identifier)))
-    
+
 #     # Replace each user-defined identifier with the corresponding replacement string
 #     for identifier, replacement in identifier_map.items():
 #         code = re.sub(r'\b{}\b'.format(re.escape(identifier)), replacement, code)
-    
+
 #     # Exclude identifiers within printf function calls
 #     def exclude_printf(match):
 #         printf_string = match.group(0)
 #         return printf_string.replace(match.group(1), ' ' * len(match.group(1)))
 
 #     code = re.sub(r'printf\s*\("([^"]*)"\)', exclude_printf, code)
-    
+
 #     return code
 
 ##########################################################################################################
@@ -246,7 +253,7 @@ if __name__ == "__main__":
     #     # Regular expression pattern to find function calls with their arguments
     #     pattern = r'\b(?:printf|puts|fputs|fprintf|fputc|fwrite|putchar)\s*\((?:[^()]|\((?:[^()]+|\([^()]*\))*\))*\);'
     #     return re.findall(pattern, code)
-    
+
     # def replace_identifiers_within_functions(text, excluded_functions, variable_mapping, detected_identifiers):
     #     for function_call in excluded_functions:
     #         # Exclude function calls that are in the excluded list
